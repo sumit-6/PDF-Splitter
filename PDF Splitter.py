72eea007eb49ea3f539e91216d31baf41ce64cb4
raghavsinghal1012 <78560533+raghavsinghal1012@users.noreply.github.com>
2021-12-18 22:16:47 +0530

    Update PDF Splitter.py

--- PDF Splitter.py
+++ PDF Splitter.py
@@ -4,6 +4,8 @@ import pandas as pd
 from tkinter import filedialog
 from tkinter import *
 from pdf2image import convert_from_path
+from goto import with_goto
+
 
 excel_path=""
 pdf_path=""
@@ -14,7 +16,7 @@ name=""
 number=""
 
 PDFFILEOPENOPTIONS = dict(defaultextension=".pdf",filetypes=[('pdf file', '*.pdf')])
-EXCELFILEOPENOPTIONS = dict(defaultextension=".xlsx",filetypes5=[('excel file', '*.xlsx')])
+
 
 def columncheck(l):
 
@@ -37,7 +39,7 @@ def columncheck(l):
     
 def excel_path1():
     global excel_path
-    excel_path=filedialog.askopenfilename(**EXCELFILEOPENOPTIONS)
+    excel_path=filedialog.askopenfilename(filetypes=[("Excel files","*.xlsx"),("Excel files","*.csv"),("Excel files", "*.xls")])
 
 def pdf_path1():
     global pdf_path
@@ -47,22 +49,41 @@ def output_path1():
     global output_path
     output_path=filedialog.askdirectory()
 
-def start_func():
+
+@with_goto
+def get_excel():
+    label.start
     root1=Tk()
     root1.geometry('900x200')
+    
     excel_button=Button(root1,text="SELECT EXCEL FILE",bg="yellow", fg="blue",font=("Arial Bold", 15),command=excel_path1)
     excel_button.grid(row = 2, column = 0)
-    
     exit_button=Button(root1,text="NEXT",bg="yellow", fg="blue",font=("Arial Bold", 15),command=root1.destroy)
     exit_button.grid(row = 2, column = 2)
     root1.mainloop()
+    if (excel_path==""):
+        goto.start
+@with_goto
+def get_pdf():
+    label.start2
     root2=Tk()
     root2.geometry('900x200')
     pdf_button=Button(root2,text="SELECT PDF FILE",bg="yellow", fg="blue",font=("Arial Bold", 15),command=pdf_path1)
     pdf_button.grid(row = 2, column = 0)
     exit_button=Button(root2,text="NEXT",bg="yellow", fg="blue",font=("Arial Bold", 15),command=root2.destroy)
     exit_button.grid(row = 2, column = 2)
+    back_button=Button(root2,text="BACK",bg="yellow", fg="blue",font=("Arial Bold", 15),command=get_excel)
+    back_button.grid(row = 2, column = 4)
     root2.mainloop()
+    if (pdf_path==""):
+        goto.start2
+
+@with_goto
+def start_func():
+    get_excel()
+    get_pdf()
+    
+    
 
      
     
@@ -101,7 +122,7 @@ def jpg_convert():
 
         images[i].save(output_path+ '\\{0}_{1}.jpg'.format(val1,val), 'JPEG')
     
-    
+@with_goto
 def convertfile():
     global exdata
     global pdf
@@ -114,13 +135,20 @@ def convertfile():
     column_list=columncheck(column_list)
     name=column_list[0]
     number=column_list[1]
+    label.start3
     root3=Tk()
     root3.geometry('900x200')
     output_button=Button(root3,text="SELECT OUTPUT FOLDER",bg="yellow", fg="blue",font=("Arial Bold", 15),command=output_path1)
     output_button.grid(row = 2, column = 0)
-    exit_button=Button(root3,text="Exit",bg="yellow", fg="blue",font=("Arial Bold", 15),command=root3.destroy)
+    exit_button=Button(root3,text="NEXT",bg="yellow", fg="blue",font=("Arial Bold", 15),command=root3.destroy)
     exit_button.grid(row = 2, column = 2)
+    back_button=Button(root3,text="BACK",bg="yellow", fg="blue",font=("Arial Bold", 15),command=get_pdf)
+    back_button.grid(row = 2, column = 4)
     root3.mainloop()
+    if(output_path==""):
+        goto.start3
+
+    
     root4=Tk()
     root4.geometry('900x200')
     PDF_button=Button(root4,text="CLICK TO CONVERT IN PDF",bg="yellow", fg="blue",font=("Arial Bold", 15),command=pdf_convert)
